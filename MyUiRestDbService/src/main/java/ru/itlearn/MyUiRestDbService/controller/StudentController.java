114beabc96df971fd7fd0cbb59484ae5a54611c6
gripndip <it.malkov.vladislav@gmail.com>
2024-12-16 00:32:42 +0500

    lab07
    Выполненная лабораторная работа 7

---
+++ MyUiRestDbService/src/main/java/ru/itlearn/MyUiRestDbService/controller/StudentController.java
@@ -0,0 +1,61 @@
+package ru.itlearn.MyUiRestDbService.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+import ru.itlearn.MyUiRestDbService.dao.StudentRepository;
+import ru.itlearn.MyUiRestDbService.entity.Student;
+import java.util.Optional;
+
+@Slf4j
+@Controller
+public class StudentController {
+
+    private StudentRepository studentRepository;
+    @Autowired
+    public StudentController(StudentRepository studentRepository) {this.studentRepository = studentRepository;}
+    @GetMapping({"/list", "/"})
+    public ModelAndView getAllStudents() {
+        ModelAndView mav = new ModelAndView("list-students");
+        mav.addObject("students", studentRepository.findAll());
+        return mav;
+    }
+
+    @GetMapping("/addStudentForm")
+    public ModelAndView addStudentForm() {
+        ModelAndView mav = new ModelAndView("add-student-form");
+        Student student = new Student();
+        mav.addObject("student", student);
+        return mav;
+    }
+
+    @PostMapping("/saveStudent")
+    public RedirectView saveStudent(@ModelAttribute Student student) {
+        studentRepository.save(student);
+        return new RedirectView("list");
+    }
+
+    @GetMapping("/showUpdateForm")
+    public ModelAndView showUpdateForm(@RequestParam Integer studentId) {
+        ModelAndView mav = new ModelAndView("add-student-form");
+        Optional<Student> optionalStudent = studentRepository.findById(studentId);
+        Student student = new Student();
+        if (optionalStudent.isPresent()) {
+            student = optionalStudent.get();
+        }
+        mav.addObject("student", student);
+        return mav;
+    }
+
+    @GetMapping("/deleteStudent")
+    public RedirectView deleteStudent(@RequestParam Integer studentId) {
+        studentRepository.deleteById(studentId);
+        return new RedirectView("list");
+    }
+}
